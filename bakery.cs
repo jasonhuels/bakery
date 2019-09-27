dcf77e5d7ab35517395428b44425f60329b26047
Jason Huels <jasonhuels@yahoo.com>
2019-09-27 10:27:11 -0700

    add functions for shopping, checking cart, and checking menu

--- bakery.cs
+++ bakery.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using BreadNS;
 using PastryNS;
@@ -7,55 +8,87 @@ class Program
 {
     static void Main()
     {
-        
-        bool shopping = true;
+        string InputString;
+        Dictionary<string, int> Cart = new Dictionary<string, int>() { {"bread", 0}, {"pastry", 0} };   
+        bool Shopping = true;
 
         Console.WriteLine("\nWelcome to Pierre's Bakery!\n");
-        while(shopping)
+        while(Shopping)
         {
-            Console.WriteLine("Would you like to see our menu? (y/n)");
-            ConsoleKey inputKey = Console.ReadKey(true).Key;
-            if(inputKey == ConsoleKey.N)
+            Console.WriteLine("Would you like to do?");
+            Console.WriteLine("[menu, cart, shop, checkout, leave]");
+            InputString = Console.ReadLine();
+            switch(InputString.ToLower())
             {
-                shopping = false;
-                break;
-            }
-            else if(inputKey == ConsoleKey.Y)
-            {
-                PrintMenu();
-            }
-            
-           
+                case "leave":
+                    Shopping = false;
+                    break;
+                case "menu":
+                    PrintMenu();
+                    break;
+                case "cart":
+                    ShowCart(Cart);
+                    break;
+                case "shop":
+                    Cart = Shop(Cart);
+                    break;
+                case "checkout":
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that command.\n");
+                    break;
+
+            }           
         }
         
     }
 
     static void PrintMenu()
-    {       
+    {   
         Console.WriteLine("\n*_*_*_*_*_*_*_Menu_*_*_*_*_*_*_*");
         Console.WriteLine("Bread: 1 Loaf: $5 /// Buy 2 get 1 Free.");
-        Console.WriteLine("Pastries: 1 Pastry: $2 /// 3 Pastries: $5");        
+        Console.WriteLine("Pastries: 1 Pastry: $2 /// 3 Pastries: $5\n");        
     }
 
-    
-
-    static int AddToCart(string itemToAdd)
+    static void ShowCart(Dictionary<string, int> Cart)
     {
-        int Count = 0;
-        string inputString;
-        Console.WriteLine("How many {0} would you like?", itemToAdd);
-        inputString = Console.ReadLine();
-        try
+        Console.WriteLine("\nYou have {0} bread and {1} pastry in your cart.\n", Cart["bread"], Cart["pastry"]);
+    }    
+
+    static Dictionary<string, int> Shop(Dictionary<string, int> Cart)
+    {
+        string InputString;
+        int Quantity;
+
+        for(int i=0; i<Cart.Count; i++)
         {
-            Count = int.Parse(inputString);
-        }
-        catch(FormatException)
-        {
-            Console.WriteLine("That is not a valid quantity ") ;
+            Console.WriteLine("How many {0} would you like?", Cart.ElementAt(i).Key);
+            InputString = Console.ReadLine();
+            
+            if(int.TryParse(InputString, out Quantity))
+            {
+                if(Cart[Cart.ElementAt(i).Key] + Quantity > 0)
+                {
+                    Cart[Cart.ElementAt(i).Key] += Quantity;
+                    ShowCart(Cart);
+                }
+                else if(Cart[Cart.ElementAt(i).Key] + Quantity == 0)
+                {
+                    Console.WriteLine("Ok, you don't want any {0}.", Cart.ElementAt(i).Key);
+                }
+                else
+                {
+                    Console.WriteLine("You can't have a negative amount of {0} in your cart.", Cart.ElementAt(i).Key);
+                }
+                
+            } 
+            else
+            {
+                Console.WriteLine("Quantity must be an integer.");
+            }
         }
 
-        return Count;
+        return Cart;
     }
-
 
 }
