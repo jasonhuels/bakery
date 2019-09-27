717faf785782e64fa889737ffe9c5e73061f6cf9
Jason Huels <jasonhuels@yahoo.com>
2019-09-27 14:23:04 -0700

    refactor and branch

--- bakery.cs
+++ bakery.cs
@@ -12,6 +12,7 @@ class Program
         Dictionary<string, int> Cart = new Dictionary<string, int>() { {"bread", 0}, {"pastry", 0} };   
         bool Shopping = true;
 
+        Console.Clear();
         Console.WriteLine("\nWelcome to Pierre's Bakery!\n");
         ShowMenu();
         while(Shopping)
@@ -22,8 +23,15 @@ class Program
             switch(InputString.ToLower())
             {
                 case "leave":
-                    Console.WriteLine("Thanks for coming to Pierre's Bakery! Goodbye.");
-                    Shopping = false;
+                    if(Cart["bread"] > 0 || Cart["pastry"] > 0)
+                    {
+                        Console.WriteLine("You should probably puschase the items in your cart with the 'checkout' command first.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thanks for coming to Pierre's Bakery! Goodbye.");
+                        Shopping = false;
+                    }           
                     break;
                 case "menu":
                     ShowMenu();
@@ -41,7 +49,7 @@ class Program
                     ShowCart(Cart);
                     break;
                 case "steal":
-                    Console.WriteLine("Pierre does not take kindly to shoplifters. You are kicked out of the Bakery.");
+                    Console.WriteLine("Pierre does not take kindly to shoplifters. You are kicked out of the bakery.");
                     Shopping = false;
                     break;
                 default:
@@ -57,9 +65,7 @@ class Program
         Console.WriteLine("Bread                     Pastries");
         Console.WriteLine("1 for $5                  1 for $2");
         Console.WriteLine("Buy 2 Get 1 Free          3 for $5");
-        Console.WriteLine("\n*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*");
-        // Console.WriteLine("Bread: 1 Loaf: $5 /// Buy 2 get 1 Free.");
-        // Console.WriteLine("Pastries: 1 Pastry: $2 /// 3 Pastries: $5\n");        
+        Console.WriteLine("\n*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*");       
     }
 
     static void ShowCart(Dictionary<string, int> Cart)
